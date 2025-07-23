9c9ec4ff2b999e48ed60ccf2247e0b4e24f28ca8
Anupam <anupam.r2301@gmail.com>
2025-07-23 23:09:49 +0530

    port issue

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,12 @@ import dotenv from "dotenv"
 dotenv.config()
 import connectDB from "./config/connectDB.js"
 import app from "./app.js"
-PORT = 3000
+
 
 const startServer = async()=>{
     try {
         await connectDB();
-        const PORT = PORT || 8000
+        const PORT = 3000
         app.listen(PORT,()=>{
             console.log(`🚀 Server is running at PORT: ${PORT}`)
         })
